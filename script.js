96095fa8842f0f5e411ed1fd0ab16684ec4c9890
cdvalencias <127420819+cdvalencias@users.noreply.github.com>
2023-05-26 18:06:02 -0500

    inicio proyecto+menu

---
+++ script.js
@@ -0,0 +1,15 @@
+let menu
+let contadorUno=0
+let contadorDos=0
+let contadorTres=0
+let contadorCuatro=0
+let contadorCinco=0
+let contadorSeis=0
+
+function menuInicio() {
+
+    menu=parseInt(prompt("ingrese 1 para ordenar numeros , 2 para calcular area de un circulo , 3 para hallar el tipo de triangulo, 4 para saber si dos numeros son amigos , 5 para conocer el valor de una compra , 6 para saber si un estudiante gano una asignatura y 7 para finalizar")) 
+    return menu   
+}
+
+menuInicio()
